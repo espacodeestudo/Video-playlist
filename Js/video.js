af7cac0f4b626ec383f1f4aacf56148bb0b7173e
yuransimao <yuransimao7@gmail.com>
2023-08-27 16:53:41 +0100

    first commit

---
+++ Js/video.js
@@ -0,0 +1,32 @@
+export const Data = [
+    {
+       category: "Modulo 1",
+       playlist : [
+         {
+            video: "../Video/city_-_6782 (720p).mp4",
+            title : "city",
+         },
+         {
+            video: "../Video/istockphoto-1046965704-640_adpp_is.mp4",
+            title : "istockphoto",
+         },
+         
+
+       ] 
+    },
+    {
+        category: "Modulo 2",
+        playlist : [
+          {
+             video: "../seoul_-_21116 (720p).mp4",
+             title : "seoul 1",
+          },
+          {
+             video: "../Video/seoul_-_24603 (1080p).mp4",
+             title : "seoul 2",
+          },
+          
+ 
+        ] 
+     }
+]
