4f60fea98255443130bdf5a92d4eb4d8ca4b5ed2
yuransimao <yuransimao7@gmail.com>
2023-08-28 16:32:37 +0100

    Adding video from playlist to playVideo

--- Js/main.js
+++ Js/main.js
@@ -1,26 +1,47 @@
 import Data from "./video.js";
+const  Modulo = document.querySelector(".Modulo")
 const Playlist = document.querySelector("#Playlist")
 
-Data.forEach((item, index) => {
-    const Button = document.createElement("button")
-    Button.classList.add("Btn-Categoy")
-    Button.innerHTML=`<h2>${item.category}</h2> <i class='bx bx-chevron-down'></i>` 
 
-    
-    if (index === 0) {
-        
-        Playlist.insertBefore(Button, Playlist.firstChild);
-    } else {
-        
-        Playlist.appendChild(Button);
-    }
 
-    Button.onclick = () => LoadVideoCategory(item.category)
+async function HandleCategory(){
+    Data.forEach((item, index) => {
+        const Button = document.createElement("button")
+        Button.classList.add(`Btn-Categoy`)
+        Button.setAttribute("id",`Btn-Categoy${index}`)
+        Button.innerHTML=`<h2>${item.category}</h2> <i class='bx bx-chevron-down'></i>` 
     
-});
+        
+        if (index === 0) {
+            
+            Playlist.insertBefore(Button, Playlist.firstChild);
+        } else {
+            
+            Playlist.appendChild(Button);
+        }
+    
+        Button.onclick = () => {
+            LoadVideoCategory(item.category)
+                if(Modulo.classList.contains("Modulo")){
+                    Modulo.className= "Modulo-desative"
+                }
+                else{
+                    Modulo.className= "Modulo"
+                }
+            
+            
+            
+        }
+      
+        
+    });
+}
 
 function LoadVideoCategory(category) {
-    const  Modulo = document.querySelector(".Modulo")
+    
+    
+   
+
     Modulo.innerHTML=""
     Data.forEach(dataItem => {
         if (dataItem.category === category) {
@@ -38,9 +59,25 @@ function LoadVideoCategory(category) {
                     Container.appendChild(H2);
                     Modulo.appendChild(Container);
 
+                    Container.onclick = () => Handle(videoItem)
                     
                 
             });
         }
     });
+
+   
 }
+
+function Handle(VideoItem){
+
+    const Video = document.querySelector("#Videoplay")
+    const Description = document.querySelector("#Description")
+    Video.src = VideoItem.video
+    Description.textContent =VideoItem.title
+    
+    console.log(VideoItem, Video)
+}
+
+
+HandleCategory()
